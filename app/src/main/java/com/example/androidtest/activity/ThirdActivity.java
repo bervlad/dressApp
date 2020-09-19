756f8a5e2ccf2801f6ca07c5745349e058d500cf
macbook <berestenko.vladyslav@gmail.com>
2020-09-19 23:41:44 +0300

    UI completed

--- app/src/main/java/com/example/androidtest/activity/ThirdActivity.java
+++ app/src/main/java/com/example/androidtest/activity/ThirdActivity.java
@@ -1,12 +1,24 @@
 package com.example.androidtest.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
+
 import com.example.androidtest.R;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class ThirdActivity extends BaseActivity {
 
@@ -15,15 +27,41 @@ public class ThirdActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
         initToolbarWithNavigation("Details", false);
+        final LinearLayoutCompat desc = findViewById(R.id.desc_tab);
+        final NestedScrollView scroll = findViewById(R.id.scrollView);
+        final AppCompatImageView line =findViewById(R.id.line1);
 
-        Spinner dropdown = findViewById(R.id.spinner1);
-        String[] items = new String[]{"Size", "2", "three"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        setSpinner(R.id.spinner1, new String[]{"Size", "2", "three"});
+        setSpinner(R.id.spinner2, new String[]{"Color", "2", "three"});
+        setSpinner(R.id.spinner3, new String[]{"1", "2", "3"});
+
+        final AppCompatImageView expand = findViewById(R.id.expand);
+        expand.setRotation(180);
+
+        expand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (desc.getVisibility() == VISIBLE) {
+                    desc.setVisibility(View.GONE);
+                    line.setVisibility(VISIBLE);
+                    expand.setRotation(0);
+                } else {
+                    desc.setVisibility(VISIBLE);
+                    line.setVisibility(INVISIBLE);
+                    expand.setRotation(180);
+                    scroll.post(new Runnable() { @Override public void run() {
+                        scroll.fullScroll(View.FOCUS_DOWN); } });
+                }
+            }
+        });
+    }
+
+    public void setSpinner(int resource, String[] items) {
+        Spinner dropdown = findViewById(resource);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown.setAdapter(adapter);
-
-        Spinner dropdown2 = findViewById(R.id.spinner2);
-        String[] items2 = new String[]{"Color", "2", "three"};
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items2);
-        dropdown2.setAdapter(adapter2);
     }
 }
