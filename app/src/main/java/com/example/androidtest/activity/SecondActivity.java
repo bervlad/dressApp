74f1912d3586cf7ca78f818470bc740c2fdac00f
macbook <berestenko.vladyslav@gmail.com>
2020-10-08 21:02:07 +0300

    problem with database

--- app/src/main/java/com/example/androidtest/activity/SecondActivity.java
+++ app/src/main/java/com/example/androidtest/activity/SecondActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.androidtest.R;
@@ -22,13 +23,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SecondActivity extends BaseActivity {
-    RecyclerView recyclerView;
-    ArrayList<DressItem> items;
-    ItemRecyclerAdapter adapter;
+    private RecyclerView recyclerView;
+    private ArrayList<DressItem> items;
+    private ItemRecyclerAdapter adapter;
     private FirebaseUser mUser;
-    UserData userData;
-    AppDatabase database;
-
+    private UserData userData;
+    private AppDatabase database;
 
 
     @Override
@@ -44,30 +44,43 @@ public class SecondActivity extends BaseActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.rv_recycler);
 
+        items=new ArrayList<>();
 
- //       if (((AndroidTest)getApplication()).getItems()==null)
- //       ((AndroidTest)getApplication()).initItems();
-
- //       items = ((AndroidTest)getApplication()).getItems();
-
-//        items = new ArrayList<DressItem>();
 //
-//        items.add(new DressItem(1, R.drawable.img_1, "Hello",null, 100, 150, 3, 2));
-//        items.add(new DressItem(2, R.drawable.img_2, "Hello2", null, 140, 170, 4, 3));
-//        items.add(new DressItem(3, R.drawable.img_2, "Hello3", "Important", 10, -1, 5, 20));
-
-//        if (mUser!=null && userData!=null) {
-//            for (DressItem item:items) {
-//                if (userData.getItems(mUser).contains(item)) {
-//                    item.setLiked(true);
-//                }
-//            }
-//        }
 
 
 
-        adapterInit();
-        initData();
+        adapter = new ItemRecyclerAdapter(items, this, userData);
+        OnDressItemClickListener listener = new OnDressItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent explicitIntent = new Intent(SecondActivity.this, ThirdActivity.class);
+                explicitIntent.putExtra(Constants.EXTRA_ITEM, items.get(position));
+                startActivity(explicitIntent);
+            }
+
+            @Override
+            public UserData onHeartClick() {
+                return getUserData();
+            }
+
+        };
+
+        adapter.setListener(listener);
+        // Can be changed to any layout manager
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setAdapter(adapter);
+
+
+        database =getDatabase();
+//        adapterInit();
+          initData();
+
+        if (((AndroidTest)getApplication()).getItems()==null)
+            ((AndroidTest)getApplication()).initItems();
+
+        items = ((AndroidTest)getApplication()).getItems();
+        database.dressItemDao().insert(items);
 
 
         if (getIntent().getExtras() != null) {
@@ -100,17 +113,18 @@ public class SecondActivity extends BaseActivity {
     }
 
     private void initData() {
-        database =getDatabase();
-        if (database != null) {
+
+        if (database!=null) {
+//            ((AndroidTest)getApplication()).initItems();
+//            items = ((AndroidTest)getApplication()).getItems();
+//          //  Log.d ("TAG2", "Model "+ items.get(0).toString());
+//            database.dressItemDao().insert(items);
             database.dressItemDao().getAll().observe(this, (List<DressItem> dressItems) -> {
-                items.clear();
-                items.addAll(dressItems);
-                adapter.notifyDataSetChanged();
-            });
-        } else {
-            ((AndroidTest)getApplication()).initItems();
-            items = ((AndroidTest)getApplication()).getItems();
-            database.dressItemDao().insert(items);
+                        items.clear();
+                        items.addAll(dressItems);
+                      //  Log.d ("TAG", "Database empty: 1st item " + items.get(0).getTitle());
+                        adapter.notifyDataSetChanged();
+                    });
         }
     }
 }
